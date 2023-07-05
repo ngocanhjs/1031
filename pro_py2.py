c285ef4699124bc6e814ba738afa1eac7bdba6ae
ngocanhjs <137131379+ngocanhjs@users.noreply.github.com>
2023-07-05 23:33:53 +0700

    pro_py2.py

--- pro_py2.py
+++ pro_py2.py
@@ -13,16 +13,16 @@ data = pd.read_csv('https://raw.githubusercontent.com/ngocanhjs/1031/main/data.c
 # Create the bar chart
 df_bar = data['MAIN_PRODUCTION'].value_counts().nlargest(n=5, keep='all').sort_values(ascending=False)
 trace_bar = go.Bar(
-    x=df_bar.values,
-    y=df_bar.index,
+    y=df_bar.values,
+    x=df_bar.index,
     orientation='v',
     marker=dict(color=['goldenrod','hotpink','chocolate','lawngreen','dodgerblue'])
 )
 data_bar = [trace_bar]
 layout_bar = go.Layout(
     title='Top 5 countries with the most TV shows (1970-2020)',
-    xaxis=dict(title='Main Production'),
-    yaxis=dict(title='Number of TV shows')
+    xaxis=dict(title='Number of TV shows'),
+    yaxis=dict(title='Main Production')
 )
 fig_bar = go.Figure(data=data_bar, layout=layout_bar)
 
@@ -42,7 +42,7 @@ app.layout = dbc.Container(
 
         dbc.Row(
             [
-                html.H1('Top Countries with Most TV Shows', style={'text-align': 'center', 'color': 'black'}),
+                html.H2('Top Countries with Most TV Shows', style={'text-align': 'center', 'color': 'black'}),
                 html.P('Number of countries:'),
                 dcc.Slider(id='slider', min=1, max=10, step=1, value=5),
                 dcc.Graph(id='plot-bar', figure=fig_bar)
@@ -81,17 +81,17 @@ app.layout = dbc.Container(
 @app.callback(Output('plot-bar', 'figure'), [Input('slider', 'value')])
 def update_bar_chart(value):
     df1 = df_bar.nlargest(n=value, keep='all').sort_values(ascending=False)
-    fig_bar = go.Figure(data=go.Bar(x=df1.values, y=df1.index, orientation='v'))
-    fig_bar.update_layout(title='Top {} countries with the most TV shows'.format(value))
+    fig_bar.update_layout(title='Top {} countries that have the most TV shows in the period 1970 - 2020'.format(value))
+    fig_bar.update_traces(y=df1.values, x=df1.index)
     return fig_bar
 
 # Callback to update the box chart based on the dropdown selection
 @app.callback(Output('plot-box', 'figure'), [Input('dropdown', 'value')])
 def update_box_chart(genre_selection):
     data_subset = data.loc[data['MAIN_GENRE'] == genre_selection]
-    fig_box = px.box(data_subset, x='MAIN_GENRE', y='SCORE', color='MAIN_GENRE')
-    fig_box.update_layout(title='The box chart demonstrates the distribution of the range score of TV shows according to TV show genres')
-    return fig_box
+    # create a new figure for the genre selection
+    fig = px.box(data_subset, x="MAIN_GENRE", y="SCORE", color="MAIN_GENRE", title=f"The chart for {genre_selection} genre")
+    return fig
 
 # Run the app
 if __name__ == '_main_':
