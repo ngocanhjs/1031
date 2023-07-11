5c1e5ec5d6c7abf233573b89d2920ce247c68c74
ngocanhjs <137131379+ngocanhjs@users.noreply.github.com>
2023-07-11 09:24:55 +0700

    pro_py2.py

--- pro_py2.py
+++ pro_py2.py
@@ -48,7 +48,7 @@ fig_box = px.box(data, x="MAIN_GENRE", y="SCORE", color="MAIN_GENRE",
 med_score = data.groupby('MAIN_GENRE')['SCORE'].median().sort_values()
 sorted_genre = med_score.index.tolist()
 fig_box.update_layout(xaxis=dict(categoryorder='array', categoryarray=sorted_genre))
-
+# Create the scatter chart
 fig_scatter = px.scatter(
     data,
     x="RELEASE_YEAR",
@@ -101,30 +101,12 @@ app.layout = dbc.Container(
         dbc.Row(
             [
                 html.H2('The Distribution of Main Genre', style={'text-align': 'center', 'color': 'black'}),
-                dbc.Col(
-                    [
                         html.Hr(),
                         html.H5('THE MAIN BOX CHART', style={'text-align': 'center'}),
                         dcc.Graph(id='plot-box', figure=fig_box, style={'height': 750}),
-                    ],
-                    width={'size': 9, 'offset': 0, 'order': 2}
+            ], width={'size': 9, 'offset': 0, 'order': 2}
                 ),
-                dbc.Col(
-                    [
-                        html.Hr(),
-                        html.H5('THE SUB BOX CHART', className='text-center'),
-                        html.Hr(),
-                        html.H6('Select genre that you want to see:', className='text-center'),
-                        dcc.Dropdown(
-                            id='dropdown',
-                            options=[{"label": option, "value": option} for option in data["MAIN_GENRE"].unique()],
-                            value="drama"
-                        ),
-                        dcc.Graph(id="plot-sub-box")
-                    ]
-                )
-            ]
-        ),
+               
   dbc.Row([
         html.Hr(),
         html.H2('The scatter plot', className='text-center'),
@@ -160,14 +142,6 @@ def update_bar_chart(value):
     fig_bar.update_traces(y=df1.values, x=df1.index)
     return fig_bar
 
-# Callback to update the box chart based on the dropdown selection
-@app.callback(Output('plot-sub-box', 'figure'), [Input('dropdown', 'value')])
-def update_box_chart(genre_selection):
-    data_subset = data.loc[data['MAIN_GENRE'] == genre_selection]
-    fig = px.box(data_subset, x="MAIN_GENRE", y="SCORE", color="MAIN_GENRE",
-                 title=f"The chart for {genre_selection} genre",
-                 color_discrete_map={genre: color for genre, color in zip(data['MAIN_GENRE'].unique(), ['goldenrod','hotpink','chocolate','lawngreen','dodgerblue','darkviolet','plum','forestgreen','crimson','yellow'])})
-    return fig
 
 # Callback to update the scatter chart based on the checkbox and range slider selection
 @app.callback(Output('plot-scatter', 'figure'), [Inpuput('checkbox', 'value')]
