08222e87bc571caa40cb15f2fbfbc81aed92fa80
ngocanhjs <137131379+ngocanhjs@users.noreply.github.com>
2023-07-16 08:41:31 +0700

    pro_py2.py

--- pro_py2.py
+++ pro_py2.py
@@ -60,7 +60,7 @@ fig_pie = px.pie(
     color_discrete_sequence=['goldenrod', 'hotpink', 'chocolate', 'lawngreen', 'dodgerblue']
 )
 fig_pie.update_traces(textposition='inside', textinfo='percent+label', marker=dict(line=dict(color='white', width=1)))
-fig_pie.update_layout(height=600)
+fig_pie.update_layout(height=500)
 
 # Create the scatter plot
 fig_scatter = px.scatter(
@@ -137,7 +137,7 @@ def update_content(bar_chart_clicks, box_chart_clicks, pie_chart_clicks, scatter
 
             html.P('Number of countries:'),
 
-            dcc.Slider(id='slider', min=1, max=5, step=1, value=5),
+            dcc.Slider(id='slider', min=1, max=10, step=1, value=5),
 
             dcc.Graph(id='plot-bar'),
 
@@ -150,13 +150,12 @@ def update_content(bar_chart_clicks, box_chart_clicks, pie_chart_clicks, scatter
         return html.Div([
 
             html.H2('The Distribution of Main Genre', style={'text-align': 'center', 'color': 'black'}),
-            html.H6('Each genre is represented by a box, with the length of the box indicating the spread and variability of that particular genre. The horizontal line inside the box represents the median genre, while the whiskers extending from the box indicate the range of values.',style={'text-align': 'center', 'color': 'lightblack', 'font-style': 'italic'}),
         
 
             html.H5('THE BOX CHART'),
         
 
-            dcc.Graph(id='plot-box', figure=fig_box, style={'height': 550}),
+            dcc.Graph(id='plot-box', figure=fig_box, style={'height': 600}),
 
         ])
 
